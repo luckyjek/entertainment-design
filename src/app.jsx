d3aa503702a805c0ab874dad3f42c2bf6754553d
luckyjek <70943889+luckyjek@users.noreply.github.com>
2022-03-09 22:39:25 +0900

    add youtube-api

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,36 @@
+import { useState, useEffect } from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
 import Main from './components/main/main';
 import Info from './components/information/information';
 import Card from './components/card/card';
-import styles from './app.module.css';
 import Investors from './components/investors/investors';
+import styles from './app.module.css';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+  useEffect(()=>{
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=bts&type=video&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`,
+      requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  },[]);
+
+
   return (
     <div className={styles.app}>
       <BrowserRouter>
         <Switch>
           <Route path={['/','/main']} exact>
-            <Main />
+
+              <Main videos={videos}/>
+
           </Route>
           <Route path='/info'>
             <Info />
