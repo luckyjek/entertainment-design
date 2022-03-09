f3e0b854ddbca36d74ee086d86e4c4bde801b289
luckyjek <70943889+luckyjek@users.noreply.github.com>
2022-03-09 22:44:29 +0900

    update header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import React,{ useState } from 'react';
+import { Link } from 'react-router-dom';
+import Dropdown from '../dropdown.js/dropdown';
 import styles from './header.module.css';
 
 const Header = () => {
-    const history = useHistory();
+    const [dropdown, setDropdown] = useState(false);
+
+    const onMouseEnter = () => {
+          setDropdown(true);
+      };
+    
+    const onMouseLeave = () => {
+        setDropdown(false);
+    };
     return(
         <>
             <header className={styles.header}>
@@ -11,10 +20,23 @@ const Header = () => {
                     <img className={styles.logo} src="/images/logo.png" alt="logo" />
                     <h1 className={styles.title}>Entertainment Desing</h1>
                 </Link>
-                <nav className={styles.nav}>
-                    <Link to={`/info`} className={styles.navItem}>INFORMATION / </Link>
-                    <Link to={`/investors`} className={styles.navItem}>INVESTORS / </Link>
-                    <Link to={`/card`} className={styles.navItem}>MAKE CARD</Link>
+                <nav>
+                    <ul className={styles.nav}>
+                        <li className={styles.navItem}
+                            onMouseEnter={onMouseEnter}
+                            onMouseLeave={onMouseLeave}
+                        >
+                            <Link to={`/info`} className={`${styles.navItem}`}>INFORMATION <i className='fas fa-caret-down' />  / </Link>
+                            {dropdown && <Dropdown />}
+                            {/* {dropdown && console.log('통과')} */}
+                        </li>
+                        <li className={styles.navItem}>
+                            <Link to={`/investors`} className={styles.navItem}> INVESTORS / </Link>
+                        </li>
+                        <li className={styles.navItem}>
+                            <Link to={`/card`} className={styles.navItem}> MAKE CARD</Link>
+                        </li>
+                    </ul>
                 </nav>
             </header>
         </>
