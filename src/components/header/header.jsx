0b45cf3553f05fb224b4eb0e1a29952384d8773f
luckyjek <70943889+luckyjek@users.noreply.github.com>
2022-03-10 11:44:25 +0900

    add nav-toggle

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,45 +4,40 @@ import Dropdown from '../dropdown.js/dropdown';
 import styles from './header.module.css';
 
 const Header = () => {
-    const [click, setClick] = useState(false);
-    const handleClick = () => setClick(!click);
-
-    const closeMobileMenu = () => setClick(false);
-
     const [dropdown, setDropdown] = useState(false);
+
     const onMouseEnter = () => {
           setDropdown(true);
       };
+    
     const onMouseLeave = () => {
         setDropdown(false);
     };
     return(
         <>
-            {/* <header className={styles.header}> */}
-                <nav className={styles.navbar} >    
-                    <Link to={`/main`} className={styles.fild}>
-                        <img className={styles.logo} src="/images/logo.png" alt="logo" />
-                        <h1 className={styles.title}>Entertainment Desing</h1>
-                    </Link>
-
-                    <div className='menu-icon' onClick={handleClick}>
-                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-                    </div>    
-
-                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                        <li className={styles.navItem} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} >
-                            <Link to={`/info`} className={`${styles.navItem}`} onClick={closeMobileMenu} >INFORMATION <i className='fas fa-caret-down' />  / </Link>
+            <header className={styles.header}>
+                <Link to={`/main`} className={styles.fild}>
+                    <img className={styles.logo} src="/images/logo.png" alt="logo" />
+                    <h1 className={styles.title}>Entertainment Desing</h1>
+                </Link>
+                <nav>
+                    <ul className={styles.nav}>
+                        <li className={`nav-item ${styles.navItem}`}
+                            onMouseEnter={onMouseEnter}
+                            onMouseLeave={onMouseLeave}
+                        >
+                            <Link to={`/info`} className={`${styles.navItem}`}>INFORMATION<br/> Dropdown <i className='fas fa-caret-down' />   </Link>
                             {dropdown && <Dropdown />}
                         </li>
                         <li className={styles.navItem}>
-                            <Link to={`/investors`} className={styles.navItem}> INVESTORS / </Link>
+                            <Link to={`/investors`} className={styles.navItem}> INVESTORS  </Link>
                         </li>
                         <li className={styles.navItem}>
                             <Link to={`/card`} className={styles.navItem}> MAKE CARD</Link>
                         </li>
                     </ul>
                 </nav>
-            {/* </header> */}
+            </header>
         </>
     )
 }
