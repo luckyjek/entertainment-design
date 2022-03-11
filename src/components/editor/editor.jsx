3d87c55685c5397bd1255b3e28430e9182d6ba1e
luckyjek <70943889+luckyjek@users.noreply.github.com>
2022-03-11 17:03:47 +0900

    handle state change on editor (use on-change)

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -3,11 +3,19 @@ import CardEditForm from '../card_edit_form/card_edit_form';
 import CardAddForm from '../card_add_form/card_add_form';
 import styles from './editor.module.css';
 
-const Editor = ({cards, addCard}) => (
+const Editor = ({cards, addCard, updateCard, deleteCard}) => (
        <section className={styles.editor}>
            <h1 className={styles.title}>Editor</h1>
-            {cards.map(card => (
-                <CardEditForm key={card.id} card={card}/>
+           {/* cards는 배열이 아니므로, map할 수 있는 상태로 만들기위해 
+           Object.key()를이용 ->cards라는 오브젝트 안에있는 모든 키들을 받아와서
+           그 키를 빙글빙글 돌게할것이다.   */}
+            {/* {cards.map(card => ( */}
+            {Object.keys(cards).map(key => ( //1,2,3 받아와짐 
+                <CardEditForm 
+                key={key} 
+                card={cards[key]}// cards안에있는 키를 접근
+                updateCard={updateCard} 
+                deleteCard={deleteCard}/>
             ))}
             <CardAddForm onAdd={addCard}/>
        </section>     
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -8,46 +8,54 @@ import Preview from '../preview/preview';
 
 const Maker = () => {
     //내가기억하고싶은 것 저장
-    const [cards, setCards] = useState([
-        {
-            id:'1',
-            name:'eunkyung1',
-            title:'오늘OOO와 함께 인증샷',
-            theme:'dark',
-            message:'같이하는가치',
-            fileName:'인증샷',
-            fileURL: null
-        },
-        {
-            id:'2',
-            name:'eunkyung2',
-            title:'오늘OOO와 함께 인증샷',
-            theme:'light',
-            message:'같이하는가치',
-            fileName:'인증샷',
-            fileURL: null
-        },
-        {
-            id:'3',
-            name:'eunkyung3',
-            title:'오늘OOO와 함께 인증샷',
-            theme:'colorful',
-            message:'같이하는가치',
-            fileName:'인증샷',
-            fileURL: null
-        }
-    ]);
+    const [cards, setCards] = useState({
+    '1':  {
+    id:'1',
+    name:'eunkyung1',
+    title:'오늘OOO와 함께 인증샷',
+    theme:'dark',
+    message:'같이하는가치',
+    fileName:'인증샷',
+    fileURL: null
+    },
+    '2':{
+        id:'2',
+        name:'eunkyung2',
+        title:'오늘OOO와 함께 인증샷',
+        theme:'light',
+        message:'같이하는가치',
+        fileName:'인증샷',
+        fileURL: null
+    },
+    '3':   
+    {
+        id:'3',
+        name:'eunkyung3',
+        title:'오늘OOO와 함께 인증샷',
+        theme:'colorful',
+        message:'같이하는가치',
+        fileName:'인증샷',
+        fileURL: null
+    }
+    });
     const history = useHistory();
 
-    const addCard = card => {
-        const updated = [...cards, card];
+    const createOrUpdateCard = card => {
+        const updated = {...cards};
+        updated[card.id] = card;
+        setCards(updated);
+    }
+
+    const deleteCard = card => {
+        const updated = {...cards};
+        delete updated[card.id];
         setCards(updated);
     }
     return(
         <section className={styles.maker}>
              <Header />
              <div className={styles.container}>
-               <Editor cards={cards} addCard={addCard}/>
+               <Editor cards={cards} addCard={createOrUpdateCard} updateCard={createOrUpdateCard} deleteCard={deleteCard}/>
                <Preview cards={cards}/>
              </div>
             {/* <Footer /> */}
