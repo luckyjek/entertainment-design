548754c8d4a174ba8e9db8d31eaf2a83b2e22fcc
luckyjek <70943889+luckyjek@users.noreply.github.com>
2022-03-12 15:35:01 +0900

    update imagefile loading spinner

--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -1,9 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './image_file_input.module.css'
 
 //1.사용자에게 이름을 받는다. (왜? 파일이름을 보여줄꺼야.)
 //2.파일 업로드 및 사용자가 다른 파일을 선택했을때, 파일이 바뀐걸 알려주는 콜백함수도받아온다
 const ImageFileInput = ({imageUploader, name, onFileChange}) => {
+    const [loading, setLoading] = useState(false);
     const inputRef = useRef();
 
     const onButtonClick = (event) =>{
@@ -11,9 +12,10 @@ const ImageFileInput = ({imageUploader, name, onFileChange}) => {
         inputRef.current.click();
     }
     const onChange = async event =>{
+        setLoading(true);
         // console.log(event.target.files[0]);
         const uploaded = await imageUploader.upload(event.target.files[0])//ImageUploader서비스에 전달
-        console.log(uploaded);
+        setLoading(false);
         onFileChange({
             name: uploaded.original_filename,
             url: uploaded.url,
@@ -29,7 +31,12 @@ const ImageFileInput = ({imageUploader, name, onFileChange}) => {
             name="file" 
             onChange={onChange}
         />
-        <button className={styles.button} onClick={onButtonClick}> {name || 'No file'} </button>
+        {!loading && (
+            <button className={`${styles.button} ${name ? styles.yellow : styles.grey}`} onClick={onButtonClick}>
+                 {name || 'No file'} 
+            </button>
+        )}
+        {loading && <div className={styles.loading}></div>}
     </div>
 }
 
